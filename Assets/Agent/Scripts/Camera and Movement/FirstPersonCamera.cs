55f6c6b61bca6d94d08c59d1cdeb05e5c7858cc1
Jordan Culver <Jordan.Culver@uoit.net>
2016-09-15 11:45:01 -0400

    Added in basic first person camera and movement scripts

---
+++ Assets/Agent/Scripts/Camera and Movement/FirstPersonCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCamera : MonoBehaviour {
+
+    private Camera playerCamera;
+    private Transform playerCameraTransform;
+
+    private Vector2 MouseMovement;
+    public float MaxCameraY;
+    public float MinCameraY;
+
+    private Quaternion rot;
+
+    // Use this for initialization
+    void Start()
+    {
+        MouseMovement = new Vector2(0.0f, 0.0f);
+
+        playerCamera = Camera.main;
+        playerCameraTransform = playerCamera.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Fetch mouse movement
+        MouseMovement.x += Input.GetAxis("Mouse X");
+        MouseMovement.y += Input.GetAxis("Mouse Y");
+
+        //Clamp pitch angle
+        MouseMovement.y = Mathf.Clamp(MouseMovement.y, MinCameraY, MaxCameraY);
+
+        //Generate rotation quaternion
+        rot = Quaternion.Euler(-MouseMovement.y, MouseMovement.x, 0.0f);
+
+        playerCameraTransform.rotation = rot;
+    }
+}
---
+++ Assets/Agent/Scripts/Camera and Movement/FirstPersonMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonMovement : MonoBehaviour {
+
+    private Transform playerTransform;
+    private Rigidbody playerRigidBody;
+    private Vector3 movementDirection;
+
+    public float movementSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        movementDirection = new Vector3(0.0f, 0.0f, 0.0f);
+
+        playerTransform = Camera.main.transform;
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = (playerTransform.forward * Input.GetAxis("Vertical") + playerTransform.right * Input.GetAxis("Horizontal"));
+
+        //playerTransform.Translate(movementDirection * movementSpeed * Time.deltaTime, Space.World);
+        playerRigidBody.AddForce(movementDirection * movementSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        if (playerRigidBody.velocity.magnitude > movementSpeed)
+        {
+            playerRigidBody.velocity = playerRigidBody.velocity.normalized * movementSpeed;
+        }
+    }
+}
