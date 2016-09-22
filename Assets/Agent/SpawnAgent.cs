541d6a4ec83492d20f2698ea74bdbeac50214bb9
Jordan Culver <Jordan.Culver@uoit.net>
2016-09-22 12:59:10 -0400

    Basic networking achieved

--- Assets/Agent/SpawnAgent.cs
+++ Assets/Agent/SpawnAgent.cs
@@ -1,25 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 using System.Collections;
 
 public class SpawnAgent : MonoBehaviour
 {
-	
+
     public GameObject agent;
 
     // OnMouseUpAsButton is only called when the mouse is released over the same GUIElement or Collider as it was pressed
     public void CreateObjectAndDisableMenu()
     {
-		ClientScene.AddPlayer (0);
-        //Instantiate(agent, new Vector3(0.0f, 0.5f, -22.0f), Quaternion.identity);
+        ClientScene.AddPlayer(NetworkManager.singleton.client.connection, 0);
 
         GetComponentInParent<Canvas>().gameObject.SetActive(false);
     }
-
-    // OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider
-    public void OnMouseDown()
-    {
-		//NetworkManager.Instantiate(agent, new Vector3(0.0f, 0.5f, -22.0f), Quaternion.identity);
-		ClientScene.AddPlayer (0);
-    }
 }
+    
