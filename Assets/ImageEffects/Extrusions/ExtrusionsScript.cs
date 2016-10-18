975f646ee42a746f6c84f2100d74d9da75425897
Satlazer <satlazer@hotmail.com>
2016-10-18 02:10:55 -0400

    [ADD] Some wavy thing has potential

---
+++ Assets/ImageEffects/Extrusions/ExtrusionsScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ImageEffectAllowedInSceneView]
+[ExecuteInEditMode]
+public class ExtrusionsScript : MonoBehaviour
+{
+    public Material effectMaterial;
+
+
+
+    // OnRenderImage is called after all rendering is complete to render image
+    void Update()
+    {
+        float timeAmount = Time.time * 10.1f;
+        //float RandomNum = Random.Range(0.0f, 1.0f);
+        //effectMaterial.SetFloat("RandomNumber", RandomNum);
+        effectMaterial.SetFloat("_Extrusion", timeAmount);
+    }
+}
