6918cc4420e43c36c8cc3cda8b7c532561fc5a1b
Jordan Culver <Jordan.Culver@uoit.net>
2016-10-20 15:31:35 -0400

    Added a bouncing ball and shit. For homework.

---
+++ Assets/BallBouncingAnimation/SquashAndStretch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class SquashAndStretch : MonoBehaviour {
+
+    public bool isPlaying;
+    private Animation anim;
+    public AnimationClip SASClip;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+        SASClip.legacy = true;
+        anim.clip = SASClip;
+        anim.AddClip(SASClip, SASClip.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPlaying)
+        {
+            anim.Play(SASClip.name);
+        }
+	}
+}
