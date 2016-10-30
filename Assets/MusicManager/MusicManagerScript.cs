3b983e73bea013a4067c7f33fe40d74af00243e4
Satlazer <satlazer@hotmail.com>
2016-10-29 22:57:45 -0400

    [FIX] Oculus works correctly now [NOT] Need to fix being able to see monsters with Oculus

---
+++ Assets/MusicManager/MusicManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MusicManagerScript : MonoBehaviour
+{
+    public AudioMixerSnapshot lowIntensity;
+    public AudioMixerSnapshot HighIntensity;
+    public AudioClip[] musicChannels;
+    public AudioSource[] musicSource;
+
+    float intensity = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < musicChannels.Length; ++i)
+        { 
+            musicSource[i].clip = musicChannels[i];
+            musicSource[i].Play();
+            musicSource[i].loop = true;
+
+            //musicSource[i].volume = 0.0f;
+        }
+        //SpookyChip
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        intensity = -Mathf.Cos(Time.fixedTime * 0.05f) * 0.5f + 0.5f;
+        float intensityAdjust = intensity * musicChannels.Length;
+        for (int i = 0; i < musicChannels.Length; ++i)
+        {
+            float intensityAdjustClamp = Mathf.Clamp(intensityAdjust - i, 0.0f, 1.0f);
+            
+            musicSource[i].volume = intensityAdjustClamp;
+        }
+
+    }
+}
