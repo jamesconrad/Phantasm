a49d31019393305ffedb95404c52dbff2b3e08eb
Satlazer <satlazer@hotmail.com>
2016-10-30 02:24:24 -0400

    [FIX] Camera and music stuff

--- Assets/MusicManager/MusicManagerScript.cs
+++ Assets/MusicManager/MusicManagerScript.cs
@@ -8,8 +8,10 @@ public class MusicManagerScript : MonoBehaviour
     public AudioMixerSnapshot HighIntensity;
     public AudioClip[] musicChannels;
     public AudioSource[] musicSource;
+    public GameObject AgentObject;
+    public GameObject[] PhantomObject;
 
-    float intensity = 1.0f;
+    float intensity = 0.3f;
 
     // Use this for initialization
     void Start()
@@ -28,7 +30,37 @@ public class MusicManagerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        intensity = -Mathf.Cos(Time.fixedTime * 0.05f) * 0.5f + 0.5f;
+        AgentObject = GameObject.Find("Agent(Clone)");
+        PhantomObject[0] = GameObject.Find("Phantom(Clone)");
+
+        if (AgentObject != null)
+        {
+            float closestPhantom = Vector3.Distance(AgentObject.transform.position, PhantomObject[0].transform.position);
+            for (int i = 1; i < PhantomObject.Length; ++i)
+            {
+                closestPhantom = Mathf.Min(closestPhantom, Vector3.Distance(AgentObject.transform.position, PhantomObject[i].transform.position));
+            }
+
+            //intensity = -Mathf.Cos(Time.fixedTime * 0.05f) * 0.5f + 0.5f;
+            float intensityOld = intensity;
+            intensity = Mathf.InverseLerp(50.0f, 2.0f, closestPhantom);
+            if (intensity > intensityOld)
+            {
+                intensity = Mathf.Lerp(intensityOld, intensity, 0.1f);
+            }
+            else
+            {
+                intensity = Mathf.Lerp(intensityOld, intensity, 0.0025f);
+            }
+        }
+        else
+        {
+            intensity = 0.0f;
+        }
+        
+       
+       
+
         float intensityAdjust = intensity * musicChannels.Length;
         for (int i = 0; i < musicChannels.Length; ++i)
         {
