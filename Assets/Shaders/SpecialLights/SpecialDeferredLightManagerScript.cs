45fbf31d12c90ceb73d5e230f00da36f250a6000
Stephen Thompson <Satlazer@hotmail.com>
2016-11-30 19:11:19 -0500

    [ADD] Laptop light in the menu

--- Assets/Shaders/SpecialLights/SpecialDeferredLightManagerScript.cs
+++ Assets/Shaders/SpecialLights/SpecialDeferredLightManagerScript.cs
@@ -5,158 +5,173 @@ using System.Collections.Generic;
 
 public class SpecialLightSystem
 {
-    // Singleton
-    static SpecialLightSystem _Instance;
-    static public SpecialLightSystem instance
-    {
-        get
-        {
-            if (_Instance == null)
-                _Instance = new SpecialLightSystem();
-            return _Instance;
-        }
-    }
+	// Singleton
+	static SpecialLightSystem _Instance;
+	static public SpecialLightSystem instance
+	{
+		get
+		{
+			if (_Instance == null)
+				_Instance = new SpecialLightSystem();
+			return _Instance;
+		}
+	}
 
-    // List of lights
-    internal HashSet<SpecialLight> _Lights = new HashSet<SpecialLight>();
+	// List of lights
+	internal HashSet<SpecialLight> _Lights = new HashSet<SpecialLight>();
 
-    public void Add(SpecialLight o)
-    {
-        // Make sure the light doesn't already exist
-        Remove(o);
-        _Lights.Add(o);
-    }
-    public void Remove(SpecialLight o)
-    {
-        _Lights.Remove(o);
-    }
+	public void Add(SpecialLight o)
+	{
+		// Make sure the light doesn't already exist
+		Remove(o);
+		_Lights.Add(o);
+	}
+	public void Remove(SpecialLight o)
+	{
+		_Lights.Remove(o);
+	}
 }
 
 [ExecuteInEditMode]
 public class SpecialDeferredLightManagerScript : MonoBehaviour
 {
-    public Shader _LightShader;
-    private Material _LightMaterial;
+	public Shader _LightShader;
+	private Material _LightMaterial;
 
-    public Mesh _CubeMesh;
-    public Mesh _SphereMesh;
-
-
-
-    private struct CommandBufferEntry
-    {
-        public CommandBuffer _AfterLighting;
-        public CommandBuffer _BeforeAlpha;
-    }
-
-    // Gotta have dat list of cameras
-    private Dictionary<Camera, CommandBufferEntry> _Cameras = new Dictionary<Camera, CommandBufferEntry>();
+	public Mesh _CubeMesh;
+	public Mesh _SphereMesh;
 
 
-    public void OnDisable()
-    {
-        // Loop through all dem cameras
-        foreach (var cam in _Cameras)
-        {
-            if (cam.Key)
-            {
-                cam.Key.RemoveCommandBuffer(CameraEvent.AfterLighting, cam.Value._AfterLighting);
-                cam.Key.RemoveCommandBuffer(CameraEvent.BeforeForwardAlpha, cam.Value._BeforeAlpha);
-            }
-        }
-        Object.DestroyImmediate(_LightMaterial);
-        // Bye Bye
-    }
+
+	private struct CommandBufferEntry
+	{
+		public CommandBuffer _AfterLighting;
+		public CommandBuffer _BeforeAlpha;
+	}
+
+	// Gotta have dat list of cameras
+	private Dictionary<Camera, CommandBufferEntry> _Cameras = new Dictionary<Camera, CommandBufferEntry>();
 
 
-    public void OnWillRenderObject()
-    {
-        var act = gameObject.activeInHierarchy && enabled;
-        if (!act)
-        {
-            OnDisable();
-            return;
-        }
+	public void OnDisable()
+	{
+		// Loop through all dem cameras
+		foreach (var cam in _Cameras)
+		{
+			if (cam.Key)
+			{
+				cam.Key.RemoveCommandBuffer(CameraEvent.AfterLighting, cam.Value._AfterLighting);
+				cam.Key.RemoveCommandBuffer(CameraEvent.BeforeForwardAlpha, cam.Value._BeforeAlpha);
+			}
+		}
+		Object.DestroyImmediate(_LightMaterial);
+		// Bye Bye
+	}
 
-        var cam = Camera.current;
-        if (!cam)
-            return;
+	public void Update()
+	{
+		DrawLights();
+	}
 
-        // Create material to render lights
-        if (!_LightMaterial)
-        {
-            _LightMaterial = new Material(_LightShader);
-            _LightMaterial.hideFlags = HideFlags.HideAndDontSave;
-        }
+	public void OnWillRenderObject()
+	{
+		DrawLights();
+	}
 
-        CommandBufferEntry buf = new CommandBufferEntry();
-        if (_Cameras.ContainsKey(cam))
-        {
-            // use existing command buffers: clear them
-            buf = _Cameras[cam];
-            buf._AfterLighting.Clear();
-            buf._BeforeAlpha.Clear();
-        }
-        else
-        {
-            // create new command buffers
-            buf._AfterLighting = new CommandBuffer();
-            buf._AfterLighting.name = "Deferred custom lights";
-            buf._BeforeAlpha = new CommandBuffer();
-            buf._BeforeAlpha.name = "Draw light shapes";
-            _Cameras[cam] = buf;
+	public void DrawLights()
+	{
+		//print("Please work");
 
-            cam.AddCommandBuffer(CameraEvent.AfterLighting, buf._AfterLighting);
-            cam.AddCommandBuffer(CameraEvent.BeforeForwardAlpha, buf._BeforeAlpha);
-        }
+		var act = gameObject.activeInHierarchy && enabled;
+		if (!act)
+		{
+			OnDisable();
+			return;
+		}
 
-        //@TODO: in a real system should cull lights, and possibly only
-        // recreate the command buffer when something has changed.
+		var cam = Camera.current;
+		if (!cam)
+			return;
 
-        var system = SpecialLightSystem.instance;
+		// Create material to render lights
+		if (!_LightMaterial)
+		{
+			_LightMaterial = new Material(_LightShader);
+			_LightMaterial.hideFlags = HideFlags.HideAndDontSave;
+		}
 
-        var propParams = Shader.PropertyToID("_SpecialLightParams");
-        var propColor = Shader.PropertyToID("_SpecialLightColor");
-        Vector4 param = Vector4.zero;
-        Matrix4x4 trs = Matrix4x4.identity;
+		CommandBufferEntry buf = new CommandBufferEntry();
+		if (_Cameras.ContainsKey(cam))
+		{
+			// use existing command buffers: clear them
+			buf = _Cameras[cam];
+			buf._AfterLighting.Clear();
+			buf._BeforeAlpha.Clear();
+		}
+		else
+		{
+			// create new command buffers
+			buf._AfterLighting = new CommandBuffer();
+			buf._AfterLighting.name = "Deferred custom lights";
+			buf._BeforeAlpha = new CommandBuffer();
+			buf._BeforeAlpha.name = "Draw light shapes";
+			_Cameras[cam] = buf;
 
-        // construct command buffer to draw lights and compute illumination on the scene
-        foreach (var o in system._Lights)
-        {
-            // light parameters we'll use in the shader
-            param.x = o._Length;
-            param.y = o._Radius;
-            param.z = 1.0f / (o._Range * o._Range);
-            param.w = (float)o._LightType;
-            buf._AfterLighting.SetGlobalVector(propParams, param);
-            // light color
-            buf._AfterLighting.SetGlobalColor(propColor, o.GetLinearColor());
+			cam.AddCommandBuffer(CameraEvent.AfterLighting, buf._AfterLighting);
+			cam.AddCommandBuffer(CameraEvent.BeforeForwardAlpha, buf._BeforeAlpha);
+		}
 
-            // Draw sphere that covers light area, with shader
-            // pass that computes illumination on the scene
-            trs = Matrix4x4.TRS(o.transform.position, o.transform.rotation, new Vector3(o._Range * 2.0f, o._Range * 2.0f, o._Range * 2.0f));
-            buf._AfterLighting.DrawMesh(_SphereMesh, trs, _LightMaterial, 0, 0);
-        }
+		//@TODO: in a real system should cull lights, and possibly only
+		// recreate the command buffer when something has changed.
 
-        // construct buffer to draw light shapes themselves as simple objects in the scene
-        foreach (var o in system._Lights)
-        {
-            // Get flat light color for alpha
-            buf._BeforeAlpha.SetGlobalColor(propColor, o.GetLinearColor());
+		var system = SpecialLightSystem.instance;
 
-            // Draw the light itself as a small sphere/box (I might make the cylinder actually draw a tube later on)
+		var propParams = Shader.PropertyToID("_SpecialLightParams");
+		var propColor = Shader.PropertyToID("_SpecialLightColor");
+		Vector4 param = Vector4.zero;
+		Matrix4x4 trs = Matrix4x4.identity;
 
-            // TRS = Translation, Rotation, Scaling
-            if (o._LightType == SpecialLight.LightType.Cylinder)
-            {
-                trs = Matrix4x4.TRS(o.transform.position, o.transform.rotation, new Vector3(o._Length * 2.0f, o._Radius * 2.0f, o._Radius * 2.0f));
-                buf._BeforeAlpha.DrawMesh(_CubeMesh, trs, _LightMaterial, 0, 1);
-            }
-            else if (o._LightType == SpecialLight.LightType.Sphere)
-            {
-                trs = Matrix4x4.TRS(o.transform.position, o.transform.rotation, new Vector3(o._Radius * 2.0f, o._Radius * 2.0f, o._Radius * 2.0f));
-                buf._BeforeAlpha.DrawMesh(_SphereMesh, trs, _LightMaterial, 0, 1);
-            }
-        }
-    }
+		// construct command buffer to draw lights and compute illumination on the scene
+		foreach (var o in system._Lights)
+		{
+			// light parameters we'll use in the shader
+			param.x = o._Length;
+			param.y = o._Radius;
+			param.z = 1.0f / (o._Range * o._Range);
+			param.w = (float)o._LightType;
+			buf._AfterLighting.SetGlobalVector(propParams, param);
+			// light color
+			buf._AfterLighting.SetGlobalColor(propColor, o.GetLinearColor());
+
+			// Draw sphere that covers light area, with shader
+			// pass that computes illumination on the scene
+			trs = Matrix4x4.TRS(o.transform.position, o.transform.rotation, new Vector3(o._Range * 2.0f, o._Range * 2.0f, o._Range * 2.0f));
+			buf._AfterLighting.DrawMesh(_SphereMesh, trs, _LightMaterial, 0, 0);
+		}
+
+		// construct buffer to draw light shapes themselves as simple objects in the scene
+		foreach (var o in system._Lights)
+		{
+			if (o._Draw)
+			{
+				// Get flat light color for alpha
+				buf._BeforeAlpha.SetGlobalColor(propColor, o.GetLinearColor());
+
+				// Draw the light itself as a small sphere/box (I might make the cylinder actually draw a tube later on)
+
+				// TRS = Translation, Rotation, Scaling
+				if (o._LightType == SpecialLight.LightType.Cylinder)
+				{
+					Vector3 rotationEuler = o.transform.rotation.eulerAngles;
+					trs = Matrix4x4.TRS(o.transform.position, Quaternion.Euler(rotationEuler.x, rotationEuler.y, rotationEuler.z + 90.0f), new Vector3(o._Radius * 2.0f, o._Length * 1.0f, o._Radius * 2.0f));
+					buf._BeforeAlpha.DrawMesh(_CubeMesh, trs, _LightMaterial, 0, 1);
+				}
+				else if (o._LightType == SpecialLight.LightType.Sphere)
+				{
+					trs = Matrix4x4.TRS(o.transform.position, o.transform.rotation, new Vector3(o._Radius * 2.0f, o._Radius * 2.0f, o._Radius * 2.0f));
+					buf._BeforeAlpha.DrawMesh(_SphereMesh, trs, _LightMaterial, 0, 1);
+				}
+			}
+		}
+	}
 }
--- Assets/Shaders/SpecialLights/SpecialLight.cs
+++ Assets/Shaders/SpecialLights/SpecialLight.cs
@@ -3,66 +3,79 @@
 [ExecuteInEditMode]
 public class SpecialLight : MonoBehaviour
 {
-    public enum LightType
-    {
-        Sphere,
-        Cylinder,
-        SomethingElse   //Maybe I'll make more later
-    }
-    public LightType _LightType;
+	public enum LightType
+	{
+		Sphere,
+		Cylinder,
+		SomethingElse   //Maybe I'll make more later
+	}
+	public LightType _LightType;
 
-    public Color _Color = Color.white;
-    public float _Intensity = 1.0f;
-    public float _Range = 10.0f;
-    public float _Radius = 0.5f;
-    public float _Length = 1.0f;
+	public Color _Color = Color.white;
+	public float _Intensity = 1.0f;
+	public float _Range = 10.0f;
+	public float _Radius = 0.5f;
+	public float _Length = 1.0f;
 
-    //These are all self explanitory, _Range is the dropoff for the attenuation, _Length is only applied to the 
+	public bool _Draw = true;
+	public bool _Rainbow = false;
 
-    public void OnEnable()
-    {
-        SpecialLightSystem.instance.Add(this);
-    }
+	//These are all self explanitory, _Range is the dropoff for the attenuation, _Length is only applied to the 
 
-    public void Start()
-    {
-        SpecialLightSystem.instance.Add(this);
-    }
+	public void OnEnable()
+	{
+		SpecialLightSystem.instance.Add(this);
+	}
 
-    public void OnDisable()
-    {
-        SpecialLightSystem.instance.Remove(this);
-    }
+	public void Start()
+	{
+		SpecialLightSystem.instance.Add(this);
+	}
 
-    public Color GetLinearColor()
-    {
-        return new Color(
-            Mathf.GammaToLinearSpace(_Color.r * _Intensity),
-            Mathf.GammaToLinearSpace(_Color.g * _Intensity),
-            Mathf.GammaToLinearSpace(_Color.b * _Intensity),
-            1.0f
-        );
-    }
+	public void OnDisable()
+	{
+		SpecialLightSystem.instance.Remove(this);
+	}
 
-    public void OnDrawGizmos()
-    {
-        Gizmos.DrawIcon(transform.position, _LightType == LightType.Cylinder ? "AreaLight Gizmo" : "PointLight Gizmo", true);
-    }
-    public void OnDrawGizmosSelected()
-    {
-        Gizmos.color = new Color(0.1f, 0.7f, 1.0f, 0.6f);
-        if (_LightType == LightType.Cylinder)
-        {
-            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, new Vector3(_Length * 2, _Radius * 2, _Radius * 2));
-            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
-        }
-        else
-        {
-            Gizmos.matrix = Matrix4x4.identity;
-            Gizmos.DrawWireSphere(transform.position, _Radius);
-        }
-        Gizmos.matrix = Matrix4x4.identity;
-        Gizmos.DrawWireSphere(transform.position, _Range);
-        //It's a sphere, the orientation of it doesn't matter
-    }
+	public Color GetLinearColor()
+	{
+		return new Color(
+			Mathf.GammaToLinearSpace(_Color.r * _Intensity),
+			Mathf.GammaToLinearSpace(_Color.g * _Intensity),
+			Mathf.GammaToLinearSpace(_Color.b * _Intensity),
+			1.0f
+		);
+	}
+
+	public void Update()
+	{
+		if(_Rainbow)
+		{
+			Color temp = Color.white;
+			Color.RGBToHSV(_Color, out temp.r, out temp.g, out temp.b);
+			_Color = Color.HSVToRGB(temp.r + Time.deltaTime, temp.g, temp.b);
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+		Gizmos.DrawIcon(transform.position, _LightType == LightType.Cylinder ? "AreaLight Gizmo" : "PointLight Gizmo", true);
+	}
+	public void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color(0.1f, 0.7f, 1.0f, 0.6f);
+		if (_LightType == LightType.Cylinder)
+		{
+			Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, new Vector3(_Length * 2, _Radius * 2, _Radius * 2));
+			Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+		}
+		else
+		{
+			Gizmos.matrix = Matrix4x4.identity;
+			Gizmos.DrawWireSphere(transform.position, _Radius);
+		}
+		Gizmos.matrix = Matrix4x4.identity;
+		Gizmos.DrawWireSphere(transform.position, _Range);
+		//It's a sphere, the orientation of it doesn't matter
+	}
 }
