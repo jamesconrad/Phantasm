4266205321911248216df18d293023d8135b5770
Jordan Culver <Jordan.Culver@uoit.net>
2016-12-01 21:49:22 -0500

    Compass added for the agent

---
+++ Assets/Agent/Scripts/UI/Compass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour 
+{
+    Canvas rootCanvas;
+    RawImage texture;
+    GameObject playerReference;
+
+    public float middlePoint = 0.378f;
+    public float offset = 0.0f;
+	// Initialization
+	void Start () 
+	{
+        rootCanvas = GetComponentInParent<Canvas>();
+        playerReference = rootCanvas.GetComponentInChildren<SplashScreen>().screenOwner;
+        texture = GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        texture.uvRect = new Rect(offset + middlePoint * ((Vector3.Angle(Vector3.left, playerReference.transform.right) / 180.0f) * (Vector3.Dot(Vector3.forward, playerReference.transform.right) > 0 ? 1 : -1)), texture.uvRect.y, texture.uvRect.width, texture.uvRect.height);
+	}
+}
--- Assets/GameState/GameState.cs
+++ Assets/GameState/GameState.cs
@@ -21,11 +21,14 @@ public class GameState : NetworkBehaviour
     // Awake is called when the script instance is being loaded
     public void Awake()
     {
-        GameObject end = possibleEnds[Random.Range(0, possibleEnds.Length - 1)];
-        end.GetComponent<MeshRenderer>().sharedMaterial = new Material( endShader);
-        GameObject trigger = (Instantiate(EndGameTrigger, end.transform.position, end.transform.rotation, end.transform) as GameObject);
-        //trigger.GetComponent<CollisionEventTrigger>().OnTriggerStart.AddListener(() => { (CustomNetworkManager.singleton as CustomNetworkManager).endGame(); });
-        trigger.GetComponent<CollisionEventTrigger>().OnTriggerStart.AddListener(() => { agentReference.GetComponent<Agent>().AgentUI.GetComponentInChildren<SplashScreen>().createSplashScreen(1); });
+        if (possibleEnds.Length > 0)
+        {
+            GameObject end = possibleEnds[Random.Range(0, possibleEnds.Length - 1)];
+            end.GetComponent<MeshRenderer>().sharedMaterial = new Material(endShader);
+            GameObject trigger = (Instantiate(EndGameTrigger, end.transform.position, end.transform.rotation, end.transform) as GameObject);
+            //trigger.GetComponent<CollisionEventTrigger>().OnTriggerStart.AddListener(() => { (CustomNetworkManager.singleton as CustomNetworkManager).endGame(); });
+            trigger.GetComponent<CollisionEventTrigger>().OnTriggerStart.AddListener(() => { agentReference.GetComponent<Agent>().AgentUI.GetComponentInChildren<SplashScreen>().createSplashScreen(1); });
+        }
     }
 
     // Use this for initialization
