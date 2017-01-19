0b072eb961cdf44b5eac12b327b2c83d30fd5e67
James Conrad <james.conrad@uoit.net>
2017-01-19 15:01:44 -0500

    Added in Doors and LevelTransition prefabs. (Scene changes irrelevant)

---
+++ Assets/LevelGeneration/ScriptsSub/GoodDoor.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodDoor : MonoBehaviour {
+
+    public int swingDir = 1;
+    public float swingSpeed = 0.8f;
+    public float openLimit = 90f;
+    public Transform hinge;
+    public Transform handle;
+    public int currentState;
+    private DoorSwingState state = new Shut();
+    private bool inputSpamming = false;
+    private int prevstate;
+
+    bool active = false;
+
+
+    void Activate()
+    {
+        active = !active;
+    }
+    	
+	// Update is called once per frame
+	void Update () {
+        int curstate = state.state();
+        if (active && Input.GetKeyDown("e"))
+        {
+            if (!inputSpamming)
+            {
+                inputSpamming = true;
+                if (curstate % 2 == 1)
+                    state = new SwingingShut(swingDir, swingSpeed, openLimit, hinge, transform);
+                else
+                    state = new SwingingOpen(swingDir, swingSpeed, openLimit, hinge, transform);
+            }
+
+            //ik right hand to handle
+        }
+        else
+            inputSpamming = false;
+
+        if (curstate <= 2 || active)
+        {
+            int nextstate = state.update();
+
+            if (prevstate != nextstate)
+            {
+                switch (nextstate)
+                {
+                    case 1:
+                        state = new SwingingOpen(swingDir, swingSpeed, openLimit, hinge, transform);
+                        break;
+                    case 2:
+                        state = new SwingingShut(swingDir, swingSpeed, openLimit, hinge, transform);
+                        break;
+                    case 3:
+                        state = new Open();
+                        break;
+                    case 4:
+                        state = new Shut();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            prevstate = nextstate;
+        }
+	}
+
+
+    private class DoorSwingState
+    {
+        public virtual int update() { return -1; }
+        public virtual int state() { return -1; }
+        public DoorSwingState(int swingD, float swingS, float openL, Transform h, Transform d)
+        {
+            swingDir = swingD;
+            swingSpeed = swingS;
+            openLimit = openL;
+            hinge = h;
+            door = d;
+        }
+        public DoorSwingState() { }
+        //vars
+        protected int swingDir;
+        protected float swingSpeed;
+        protected float openLimit;
+        protected Transform hinge;
+        protected Transform door;
+    }
+
+    private class SwingingOpen : DoorSwingState
+    {
+        public override int update()
+        {
+            door.RotateAround(hinge.position, hinge.up, swingDir * swingSpeed * Time.deltaTime);
+            if (door.localRotation.eulerAngles.y >= openLimit)
+            {
+                return 3;
+            }
+            return 0;
+        }
+        public override int state() { return 1; }
+        public SwingingOpen(int swingD, float swingS, float openL, Transform h, Transform d)
+        {
+            swingDir = swingD;
+            swingSpeed = swingS;
+            openLimit = openL;
+            hinge = h;
+            door = d;
+        }
+    }
+
+    private class SwingingShut : DoorSwingState
+    {
+        public override int update()
+        {
+            door.RotateAround(hinge.position, hinge.up, -swingDir * swingSpeed * Time.deltaTime);
+            if (door.localRotation.eulerAngles.y >= 315 && door.localRotation.eulerAngles.y <= 360)
+            {
+                return 4;
+            }
+            return 0;
+        }
+        public override int state() { return 2; }
+        public SwingingShut(int swingD, float swingS, float openL, Transform h, Transform d)
+        {
+            swingDir = swingD;
+            swingSpeed = swingS;
+            openLimit = openL;
+            hinge = h;
+            door = d;
+        }
+    }
+
+    private class Open : DoorSwingState
+    {
+        public override int update()
+        {
+            return 0;
+        }
+        public override int state() { return 3; }
+    }
+
+    private class Shut : DoorSwingState
+    {
+        public override int update()
+        {
+            return 0;
+        }
+        public override int state() { return 4; }
+    }
+}
---
+++ Assets/LevelGeneration/ScriptsSub/LevelTransition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour {
+
+    public string destinationLevel;
+    public Texture2D fadeTexture; //overlay texture, probally will be the loading image
+    public float fadeSpeed = 0.8f;
+
+    private int drawDepth = -1000;//make sure overlay is overlayed
+    private float alpha = 1.0f;
+    private int fadeDir = -1;   //fade direction, fade in -1, fade out 1;
+
+	void OnGUI()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+        GUI.depth = drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+    }
+
+    //Used for timing of level load
+    public float BeginFade(int direction)
+    {
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    void OnLevelWasLoaded()
+    {
+        BeginFade(-1);
+    }
+
+    public IEnumerator Transition()
+    {
+        float fadeTime = BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(destinationLevel);
+    }
+}
