25ec02f1d5d9e0c6a8b0ad2379b68a0af0a022c7
James Conrad <james.conrad@uoit.net>
2017-02-13 19:13:02 -0500

    [ADD] In world hacker cameras that should follow their camera. Scene changes irrelevant. Missing camera textures.

---
+++ Assets/Hacker/WorldCameraOrientation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCameraOrientation : MonoBehaviour {
+
+    private Transform cam;
+    private Vector3 brot;
+
+	// Use this for initialization
+	void Start () {
+        cam = transform.parent.transform.parent.transform.parent.transform;
+        brot = new Vector3(90, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 crot = cam.eulerAngles;
+        Vector3 rot = crot + brot;
+        transform.rotation.Equals(Quaternion.Euler(rot));
+	}
+}
