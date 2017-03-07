7412fd6486d795e19eaa94feaaa52b618677055c
Jordan Culver <Jordan.Culver@uoit.net>
2017-03-07 13:30:03 -0500

    Starting rework of Network Manager. Moving menu functionality into a menu manager.

---
+++ Assets/NetworkManager/PhaNetworking/PhaNetworkingAPI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Net;
+using UnityEngine;
+
+public class PhaNetworkingAPI : MonoBehaviour {
+	
+
+	public static System.IntPtr mainSocket;
+	const int mainPort = 8889;
+	public static IPAddress hostAddress;
+	public static StringBuilder targetIP = new StringBuilder("127.0.0.1");
+
+	[DllImport("PhaNetworking", EntryPoint="Initialize", CallingConvention = CallingConvention.Cdecl)]
+	public static extern System.IntPtr InitializeNetworking(int port = mainPort);
+
+	[DllImport("PhaNetworking", EntryPoint="Send", CallingConvention = CallingConvention.Cdecl)]
+	public static extern int SendTo(System.IntPtr givenSocket, StringBuilder buffer, int bufferLength, StringBuilder givenAddress, int givenPort = mainPort);
+	
+	[DllImport("PhaNetworking", EntryPoint="Receive", CallingConvention = CallingConvention.Cdecl)]
+	public static extern int ReceiveFrom(System.IntPtr givenSocket, StringBuilder receiveBuffer, int bufferLength);
+	
+	[DllImport("PhaNetworking", EntryPoint="GetRemoteAddress", CallingConvention = CallingConvention.Cdecl)]
+	public static extern void GetRemoteAddress(System.IntPtr givenSocket, StringBuilder buffer, int bufferLength);
+	
+	[DllImport("PhaNetworking", EntryPoint="CloseDown", CallingConvention = CallingConvention.Cdecl)]
+	public static extern void ShutDownNetwork(System.IntPtr givenSocket);
+
+}
---
+++ Assets/NetworkManager/PhaNetworking/Updaters/PhantomSend.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhantomSend : PhaNetworkingMessager {
+
+	
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		SendEnemyUpdate(transform.position, transform.rotation, PhaNetworkingAPI.targetIP);
+	}
+}
