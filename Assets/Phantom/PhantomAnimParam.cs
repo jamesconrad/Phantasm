a29de7d172da05e934b8d2e90b5a12d65d01d2c8
James Conrad <james.conrad@uoit.net>
2017-03-20 13:29:20 -0400

    [ADD] New Phantom model and animations.

---
+++ Assets/Phantom/PhantomAnimParam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PhantomAnimParam : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Animator anim;
+    Vector3 last;
+
+	// Use this for initialization
+	void Start () {
+        agent = transform.parent.GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 d = last - transform.position;
+        anim.SetFloat("mov", agent.velocity.magnitude);
+	}
+}
