65e3341ff1a6df261505f589f4aa3ed26b9c74cb
Satlazer <Satlazer@hotmail.com>
2017-03-20 21:49:09 -0400

    [MOD] Modified Music manager range

--- Assets/MusicManager/MusicManagerScript.cs
+++ Assets/MusicManager/MusicManagerScript.cs
@@ -39,6 +39,9 @@ public class MusicManagerScript : MonoBehaviour
     const float timeToWaitTillSearch = 10.0f;
     float timeSinceLastSearch = timeToWaitTillSearch;
 
+    float minDistance = 1.0f;
+    float maxDistance = 25.0f;
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -68,14 +71,15 @@ public class MusicManagerScript : MonoBehaviour
 
             //intensity = -Mathf.Cos(Time.fixedTime * 0.05f) * 0.5f + 0.5f;
             float intensityOld = intensity;
-            intensity = Mathf.InverseLerp(50.0f, 2.0f, closestPhantom);
+            intensity = Mathf.InverseLerp(maxDistance, minDistance, closestPhantom);
+            intensity *= intensity;
             if (intensity > intensityOld)
             {
                 intensity = Mathf.Lerp(intensityOld, intensity, 0.1f);
             }
             else
             {
-                intensity = Mathf.Lerp(intensityOld, intensity, 0.0025f);
+                intensity = Mathf.Lerp(intensityOld, intensity, 0.0125f);
             }
         }
         else
