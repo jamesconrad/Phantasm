69f699c4223994b96b167d59b3fda404bd0f79c2
Satlazer <Satlazer@hotmail.com>
2017-04-01 16:37:33 -0400

    [FIX] Heartbeat now beats

--- Assets/Agent/Heartbeat.cs
+++ Assets/Agent/Heartbeat.cs
@@ -5,13 +5,10 @@ using XInputDotNetPure; // Required in C#
 
 public class Heartbeat : MonoBehaviour {
 
-	private float leftMotor;
-	private float rightMotor;
-	private GamePadState state;
-	private int rumbleTimer;
 	private float beatSpeed;
 	public float beatIntensity;
 	public bool beatActive;
+	private bool vTest;
 
 
 
@@ -27,49 +24,54 @@ public class Heartbeat : MonoBehaviour {
 
 	}
 
-
 	IEnumerator Beat(){
-		while (true) {
-			
-			//Time between beat pairs
-			beatSpeed = Mathf.Lerp (1.1f, 0.25f, beatIntensity);
-			beatSpeed = Mathf.Clamp (beatSpeed, 0, 1);
-			yield return new WaitForSeconds (beatSpeed);
 
-			//If the heartbeat is turned on
-			if (beatActive) {
-				//low beat
-				float timer = 0.1f;
-				while (timer >= 0) {
-					GamePad.SetVibration (PlayerIndex.One, 1.0f, 1.0f);
-					timer -= Time.deltaTime;
-				}
+		//vTest is set to false at the start, then true at the end,
+		//	making it so that update will not ruin our fun.
+		vTest = false;
 
-				//Time between low/high beat
-				yield return new WaitForSeconds (0.2f);
+		//Time between beat pairs of beats
+		beatSpeed = Mathf.Lerp (1.1f, 0.25f, beatIntensity);
+		beatSpeed = Mathf.Clamp (beatSpeed, 0, 1);
+		yield return new WaitForSeconds (beatSpeed);
 
-				//high beat
-				timer = 0.12f;
-				while (timer >= 0) {
-					GamePad.SetVibration (PlayerIndex.One, 1.0f, 1.0f);
-					timer -= Time.deltaTime;
-				}
-			}
+		//low beat
+		float timer = 0.3f;
+		while (timer >= 0) {
+			GamePad.SetVibration (PlayerIndex.One, 1.0f, 1.0f);
+			timer -= Time.deltaTime;
 		}
+		GamePad.SetVibration (PlayerIndex.One, 0.0f, 0.0f);
+
+		//Time between low/high beat
+		yield return new WaitForSeconds (0.2f);
+
+		//high beat
+		//print ("3");
+		timer = 0.4f;
+		while (timer >= 0) {
+			GamePad.SetVibration (PlayerIndex.One, 1.0f, 1.0f);
+			timer -= Time.deltaTime;
+		}
+		GamePad.SetVibration (PlayerIndex.One, 0.0f, 0.0f);
+
+		//Again, vTest is set back to true,
+		//	meaning it is now time to do this thing again
+		vTest = true;
 	}
 		
 
-	// Use this for initialization
 	void Start () {
-		//StartCoroutine (lowBeat ());
-		rumbleTimer = 50;
-
-		StartCoroutine (Beat ());
+		vTest = true;
 	}
 
 
-	//// Update is called once per frame
-	//void Update () {
-	//
-	//}
+	void Update () {
+		//the "vTest" should stop it from updating always
+		//	(since waiting is part of a coroutine and such)
+		//and, of course, it wont do a gosh dern thing if beat active is off.
+		if (vTest && beatActive){
+			StartCoroutine (Beat ());
+		}
+	}
 }
