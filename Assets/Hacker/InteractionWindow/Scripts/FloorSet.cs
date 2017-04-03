080c07b923d3a24108e05be352aca2feb668b777
James Conrad <james.conrad@uoit.net>
2017-04-02 22:32:31 -0400

    [ADD] Viewmode buttons, needs textures.

---
+++ Assets/Hacker/InteractionWindow/Scripts/FloorSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorSet : MonoBehaviour {
+
+    public HackerInteractionWindowSetup hiws;
+    public Text t;
+
+	// Use this for initialization
+	void Start ()
+    {
+        t = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        t.text = hiws.viewFloor.ToString();
+	}
+}
---
+++ Assets/Hacker/InteractionWindow/Scripts/ViewModeSwap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewModeSwap : MonoBehaviour {
+
+    private HackerVisionScript[] hvs;
+    public HackerVisionScript.HackerVisionMode buttonMaterial;
+    public RawImage highlight;
+
+    public void Start()
+    {
+        hvs = FindObjectsOfType<HackerVisionScript>();
+    }
+
+    public void OnClick()
+    {
+        for (int i = 0; i < hvs.Length; i++)
+        {
+            hvs[i].Vision = buttonMaterial;
+            hvs[i].timeSinceSwap = 0.0f;
+
+            if (buttonMaterial == HackerVisionScript.HackerVisionMode.Normal)
+
+            {
+                hvs[i].cameraModeArray = 1;
+                hvs[i].filmGrainBarrel = hvs[i].barrelDistortAmount.normal;
+                hvs[i].filmGrainBarrelZoom = hvs[i].barrelDistortZoomAmount.normal;
+            }
+            else if (buttonMaterial == HackerVisionScript.HackerVisionMode.Night)
+            {
+                hvs[i].cameraModeArray = 1;
+                hvs[i].filmGrainBarrel = hvs[i].barrelDistortAmount.night;
+                hvs[i].filmGrainBarrelZoom = hvs[i].barrelDistortZoomAmount.night;
+            }
+            else if (buttonMaterial == HackerVisionScript.HackerVisionMode.Sonar)
+            {
+                hvs[i].cameraModeArray = 3;
+                hvs[i].filmGrainBarrel = hvs[i].barrelDistortAmount.sonar;
+                hvs[i].filmGrainBarrelZoom = hvs[i].barrelDistortZoomAmount.sonar;
+            }
+            else if (buttonMaterial == HackerVisionScript.HackerVisionMode.Thermal)
+            {
+                hvs[i].cameraModeArray = 2;
+                hvs[i].filmGrainBarrel = hvs[i].barrelDistortAmount.thermal;
+                hvs[i].filmGrainBarrelZoom = hvs[i].barrelDistortZoomAmount.thermal;
+            }
+        }
+
+        highlight.transform.position = transform.position;
+    }
+}
